0bee8ddaba15115cc13300672679ffaa4d95f1c6
mrz1836 <mrz1818@gmail.com>
2022-01-23 13:44:13 -0500

    Broke up client into multiple interfaces

--- interface.go
+++ interface.go
@@ -2,18 +2,27 @@ package mattercloud
 
 import "context"
 
-// ClientInterface is the MatterCloud client interface
-type ClientInterface interface {
-	Network() NetworkType
+// AddressService is the MatterCloud address related requests
+type AddressService interface {
 	AddressBalance(ctx context.Context, address string) (balance *Balance, err error)
 	AddressBalanceBatch(ctx context.Context, addresses []string) (balances []*Balance, err error)
 	AddressHistory(ctx context.Context, address string) (history *History, err error)
 	AddressHistoryBatch(ctx context.Context, addresses []string) (history *History, err error)
 	AddressUtxos(ctx context.Context, address string) (utxos []*UnspentTransaction, err error)
 	AddressUtxosBatch(ctx context.Context, addresses []string) (utxos []*UnspentTransaction, err error)
+}
+
+// TransactionService is the MatterCloud transaction related requests
+type TransactionService interface {
 	Broadcast(ctx context.Context, rawTx string) (response *BroadcastResponse, err error)
 	Transaction(ctx context.Context, tx string) (transaction *Transaction, err error)
 	TransactionBatch(ctx context.Context, txIDs []string) (transactions []*Transaction, err error)
-	Request(ctx context.Context, endpoint, method string,
-		payload []byte) (response string, err error)
+}
+
+// ClientInterface is the MatterCloud client interface
+type ClientInterface interface {
+	AddressService
+	TransactionService
+	Network() NetworkType
+	Request(ctx context.Context, endpoint, method string, payload []byte) (response string, err error)
 }
